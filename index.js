04d58ec2646bfb5ed9659523a3a08c969711f489
Heera Singh Lodhi <heera933@gmail.com>
2022-11-03 22:38:47 +0530

    NEW APP

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send('worked');
+
+    res.end()
+});
+
+// Establishing the port
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+    console.log('Server has been started on port: 3000');
+})
